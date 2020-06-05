4b5962063d1d7b6177731177928db12658a8c9d6
Michelle Glass <mglass@Michelles-MacBook-Air.local>
2020-06-04 21:50:49 -0500

    phase 2 complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,24 @@ import React from 'react';
 import GhibliCard from './components/GhibiliCard'
 
 class App extends React.Component {
-        state = {
-            movies: [],
-            id: '',
-            title: '',
-            description: '',
-            director: '',
-            producer: '',
-            release_date: '',
-            rt_scote: '',
-            isLoaded: false,
-        }
-    
+    state = {
+        movies: [],
+        id: '',
+        title: '',
+        description: '',
+        director: '',
+        producer: '',
+        release_date: '',
+        rt_scote: '',
+        isLoaded: false,
+    }
+
     componentDidMount() {
         console.log('hello')
         fetch("https://ghibliapi.herokuapp.com/films")
             .then(res => res.json())
             .then(json => {
                 this.setState({
-                    isLoaded: true,
                     movies: json,
                 })
             })
@@ -44,13 +43,22 @@ class App extends React.Component {
             );
         } else {
             return (
-                <h1>Please wait while page loads...</h1>
+                <div className="col-md-8">
+                    <div className="card my-2 shadow" >
+                        <img className="card-img-top" src="https://upload.wikimedia.org/wikipedia/en/thumb/c/ca/Studio_Ghibli_logo.svg/1200px-Studio_Ghibli_logo.svg.png" alt="https://ghibliapi.herokuapp.com/images/logo.svg"></img>
+                        <div className="card-body">
+                            <p className="card-text">A simple website to display information about the Studio Ghibli Films.</p>
+                            <button onClick={() => this.setState({ isLoaded: true })} className="btn btn-dark">Load Films</button>
+                        </div>
+                    </div>
+                </div>
             )
-
         }
-
-
     }
 }
+
+
+
+
 
 export default App;
